8a9d61ab5034b46c77c1bfae150c214e3094db85
ECS <ecs@ECSs-MacBook-Pro.local>
2018-12-04 16:14:27 +0700

    Commit

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 import React, {Component} from 'react';
 import AppNavigator from './src/AppNavigator';
 
-export default class App extends Component {
+export default class App extends Component {  
   render() {
     return (
       <AppNavigator />
