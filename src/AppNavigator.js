5e47e9f48b410e20c08db0dfdec13e86c3801c19
ECS <ecs@ECSs-MacBook-Pro.local>
2018-12-06 10:48:52 +0700

    Commit

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,7 +1,7 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import MusicList from './MusicList';
 
-export default createStackNavigator(
+const AppNavigator = createStackNavigator(
   {
     List: { screen: MusicList }
   },
@@ -18,3 +18,4 @@ export default createStackNavigator(
     },
   }
 );
+export default createAppContainer(AppNavigator);
